2c2f1795385c1153a699ef609c2fa79f472dd0cf
mrdang <khacquyet.dang.pro@gmail.com>
2019-01-13 20:37:56 +0100

    séparér en composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { HeaderComponent } from './header/header.component';
+import { EducationComponent } from './education/education.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AboutmeComponent,
+    HeaderComponent,
+    EducationComponent,
+    ContactComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
